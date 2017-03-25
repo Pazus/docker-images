62d8431b4f38896974eae9e6ef57bc17b631b70c
Pazus <pazzzus@gmail.com>
2017-03-25 22:19:36 +0300

    попытка создать без PDB

--- OracleDatabase/dockerfiles/12.2.0.1/runOracle.sh
+++ OracleDatabase/dockerfiles/12.2.0.1/runOracle.sh
@@ -154,7 +154,7 @@ else
    rm -f $ORACLE_HOME/network/admin/tnsnames.ora
    
    # Create database
-   $ORACLE_BASE/$CREATE_DB_FILE $ORACLE_SID $ORACLE_PDB;
+   $ORACLE_BASE/$CREATE_DB_FILE $ORACLE_SID;
    
    # Move database operational files to oradata
    moveFiles;
