4b6620ff7fb6fb3f43d50baaee74cccc64874d06
Pazus <pazzzus@gmail.com>
2017-03-25 22:05:23 +0300

    first try

---
+++ OracleDatabase/dockerfiles/12.2.0.1/cleanup.sh
@@ -0,0 +1,5 @@
+rm -rf $ORACLE_HOME/apex
+rm -rf $ORACLE_HOME/inventory/backup
+rm -rf $ORACLE_HOME/sqldeveloper
+rm -rf $ORACLE_HOME/assisnants/dbca/templates
+rm -rf $ORACLE_HOME/assistants/dbca/templates
