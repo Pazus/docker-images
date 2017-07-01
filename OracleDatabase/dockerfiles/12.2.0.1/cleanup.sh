2eeb566eee76908ab5e0eeaa80a9ddb434e6979a
Pazus <pazzzus@gmail.com>
2017-07-01 23:06:08 +0300

    fixed typo

--- OracleDatabase/dockerfiles/12.2.0.1/cleanup.sh
+++ OracleDatabase/dockerfiles/12.2.0.1/cleanup.sh
@@ -1,5 +1,5 @@
 rm -rf $ORACLE_HOME/apex
 rm -rf $ORACLE_HOME/inventory/backup
 rm -rf $ORACLE_HOME/sqldeveloper
-rm -rf $ORACLE_HOME/assisnants/dbca/templates
+rm -rf $ORACLE_HOME/assistants/dbca/templates
 #mkdir -p $PDB_BASE_DIR
